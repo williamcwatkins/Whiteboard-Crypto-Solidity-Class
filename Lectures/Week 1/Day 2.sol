6149ad98509bab6383c5b6e069caed2e75896094
williamcwatkins <william.watkins@colorado.edu>
2023-01-19 00:02:28 -0600

    Day 2

---
+++ Lectures/Week 1/Day 2.sol
@@ -0,0 +1,7 @@
+// comment bb
+/* long form comment
+here too
+done */
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
---
+++ Lectures/Week 1/Day 3.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+contract helloWorld{
+    string public greet = "Hello World!";
+    // type accessModifier name = value
+    // (what) (where) (who)
+}
